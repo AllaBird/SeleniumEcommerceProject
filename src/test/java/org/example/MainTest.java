153183f1e8bb44c55113f578bacb982e5d1f8f0d
AllaBird <allavorobieva@yahoo.com>
2024-08-22 22:58:18 -0400

    Create MainTest

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class MainTest {
+
+    @BeforeMethod
+    public void setUp() {
+    }
+
+    @AfterMethod
+    public void tearDown() {
+    }
+
+    @Test
+    public void testMain() {
+    }
+}
