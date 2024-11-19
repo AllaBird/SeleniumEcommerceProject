41e85d12abd53a314c3688964106ee88d07435b1
AllaBird <allavorobieva@yahoo.com>
2024-11-19 01:28:08 -0500

    Create

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,21 +1,42 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
 public class MainTest {
+    private WebDriver driver;
 
-    @BeforeMethod
+    @BeforeClass
     public void setUp() {
-    }
-
-    @AfterMethod
-    public void tearDown() {
+        // Автоматически загружает драйвер
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
     }
 
     @Test
-    public void testMain() {
+    public void testGoogleSearch() {
+        // Открываем страницу Google
+        driver.get("https://www.google.com");
+
+        // Проверяем, что элемент поиска присутствует
+        boolean isSearchBoxPresent = driver.findElement(By.name("q")).isDisplayed();
+        Assert.assertTrue(isSearchBoxPresent, "Поиск не найден на странице");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        // Закрываем драйвер
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
